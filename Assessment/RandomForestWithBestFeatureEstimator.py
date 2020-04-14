d04ec02f0c866c1e89d979bb84f8bf7fae6841b1
Inna Williams <nogia22@gmail.com>
2020-04-14 11:53:27 -0400

    Logistic Regression VS Ranndom Forest with Best Feature Estimator

---
+++ Assessment/RandomForestWithBestFeatureEstimator.py
@@ -0,0 +1,186 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as  sns
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+from sklearn import tree
+from sklearn import linear_model
+
+def get_float_rows(dataset) :
+    col_float = []
+    col_object = []
+    for i in dataset.columns:
+        if dataset[i].dtypes == np.float:
+            col_float.append(i)
+        elif dataset[i].dtypes == np.object:
+            col_object.append(i)
+    return col_float,col_object
+# Load in data
+df_train = pd.read_csv('exercise_02_train.csv')
+x_test_set  = pd.read_csv('exercise_02_test.csv')
+
+# check data types before training
+
+df_train_types = df_train.dtypes
+x_test_types = df_train.dtypes
+
+x_train_set = df_train.iloc[:,0:len(df_train.columns)-1]
+
+col_float,col_object = get_float_rows(x_train_set)
+
+x_train_set[col_object[2]] =x_train_set[col_object[2]].replace('[\$,]', '', regex=True).astype(float)
+x_train_set[col_object[3]] =x_train_set[col_object[3]].replace('[\%,]', '', regex=True).astype(float)
+categorized = [col_object[0],col_object[1],col_object[4],col_object[5]]
+for i in range(0,len(categorized)):
+    codes, uniques = pd.factorize(x_train_set[categorized[i]])
+    print(type(list(codes)))
+    x_train_set[categorized[i]] = list(codes)
+
+x_test_set[col_object[2]] =x_test_set[col_object[2]].replace('[\$,]', '', regex=True).astype(float)
+x_test_set[col_object[3]] =x_test_set[col_object[3]].replace('[\%,]', '', regex=True).astype(float)
+categorized = [col_object[0],col_object[1],col_object[4],col_object[5]]
+for i in range(0,len(categorized)):
+    codes, uniques = pd.factorize(x_test_set[categorized[i]])
+    print(type(list(codes)))
+    x_test_set[categorized[i]] = list(codes)
+
+x_train = x_train_set
+x_test = x_test_set
+###############################################################################
+# using Nearest neighbors imputation
+###############################################################################
+import numpy as np
+from sklearn.impute import KNNImputer
+
+x_train = x_train.replace(x_train.isnull(), np.nan)
+x_test = x_test.replace(x_train.isnull(), np.nan)
+
+miss_number_train = (x_train.isnull().sum() > 0).astype(np.int64).sum()
+print("Before removing impurities = ",miss_number_train)
+miss_number_test = (x_test.isnull().sum() > 0).astype(np.int64).sum()
+print("Before removing impurities = ",miss_number_test)
+
+imputer = KNNImputer(n_neighbors=1)
+xtrain_filled = imputer.fit_transform(x_train)
+xtest_filled = imputer.fit_transform(x_test)
+
+x_train = pd.DataFrame(data=xtrain_filled, columns=x_train.columns)
+x_test = pd.DataFrame(data=xtest_filled, columns=x_test.columns)
+
+
+miss_number_train = (x_train.isnull().sum() > 0).astype(np.int64).sum()
+print("After removing impurities = ",miss_number_train)
+miss_number_test = (x_test.isnull().sum() > 0).astype(np.int64).sum()
+print("Before removing impurities = ",miss_number_test)
+
+X=x_train
+y = df_train['y']
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+#############################################################################
+##########  ML Random Forest with Optimal Parameters found :  ###############
+##########  n estimators = 200, depth = 100                   ###############
+#############################################################################
+print('Start  Random Forest Algorithm')
+rfc = RandomForestClassifier(n_estimators=200, max_depth=100, max_features='sqrt')
+rfc.fit(X_train,y_train)
+rfc_predict = rfc.predict(X_test)
+####################  Statistics Results    #################################
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+kf = KFold(shuffle=True, n_splits=5)
+rfc_cv_score = cross_val_score(rfc, X, y, cv=kf, scoring='accuracy')
+
+from sklearn import metrics
+print('=============== Accuracy Score ===============')
+print(metrics.accuracy_score(y_test, rfc_predict))
+print('=============== Total Counts ================')
+print(y_test.value_counts())
+print('=============== y_test.mean ===============')
+print(y_test.mean())
+print('=============== 1 - y_test.mean ===============')
+print(1 - y_test.mean())
+print('=============== the first 25 true and predicted responses ===============')
+print('True:', y_test.values[0:25])
+print('False:',rfc_predict[0:25])
+########################################################################
+confusion = metrics.confusion_matrix(y_test, rfc_predict)
+print('=============== Confusion Matrix ===============')
+print(confusion)
+TP = confusion[1, 1]
+TN = confusion[0, 0]
+FP = confusion[0, 1]
+FN = confusion[1, 0]
+# use float to perform true division, not integer division
+print((TP + TN) / float(TP + TN + FP + FN))
+print(metrics.accuracy_score(y_test, rfc_predict))
+classification_error = (FP + FN) / float(TP + TN + FP + FN)
+print('=============== classification error ===============')
+print(classification_error)
+print(1 - metrics.accuracy_score(y_test, rfc_predict))
+sensitivity = TP / float(FN + TP)
+print('=============== sensitivity =============== ')
+print(sensitivity)
+print(metrics.recall_score(y_test, rfc_predict))
+specificity = TN / (TN + FP)
+print("=============== specifisity =============== ")
+print(specificity)
+false_positive_rate = FP / float(TN + FP)
+print("=============== false positive ===============")
+print(false_positive_rate)
+print(1 - specificity)
+false_negative_rate = FN / float(FN + TP)
+print("=============== false Negative ===============")
+print(false_negative_rate)
+print(1 - sensitivity)
+precision = TP / float(TP + FP)
+print("=============== presision ===============")
+print(precision)
+print(metrics.precision_score(y_test, rfc_predict))
+print("================== Classification Report ===============")
+print(classification_report(y_test, rfc_predict))
+print('Cross Validation Score = ')
+print(rfc_cv_score)
+print("=== Mean Of Cross Valiation Score ===")
+print( rfc_cv_score.mean())
+##################################3#########################################
+from sklearn.metrics import plot_confusion_matrix
+# Plot non-normalized confusion matrix
+titles_options = [("Confusion matrix, without normalization", None),
+                  ("Normalized confusion matrix", 'true')]
+for title, normalize in titles_options:
+    disp = plot_confusion_matrix(rfc, X_test, y_test,
+                                 display_labels= ['0','1'],
+                                 cmap=plt.cm.Blues,
+                                 normalize=normalize)
+    disp.ax_.set_title(title)
+plt.show()
+#########################################################################
+y_pred_proba = rfc.predict_proba(X_test)[:, 1]
+fpr, tpr, _ = metrics.roc_curve(y_test,  y_pred_proba)
+auc = metrics.roc_auc_score(y_test, y_pred_proba)
+plt.plot(fpr,tpr,label="data 1, auc="+str(auc))
+plt.legend(loc=4)
+plt.show()
+import matplotlib.pyplot as plt
+plt.hist(y_pred_proba, bins=8)
+plt.xlim(0,1)
+plt.title('Histogram of predicted probabilities')
+plt.xlabel('Predicted probability of 1 ')
+plt.ylabel('Frequency')
+plt.show()
+#########################################################################
+# Calculate and Output Predicted values on exercise_02_test.csv
+y_pred_class = np.trunc(rfc.predict(x_test))
+y_pred_class = y_pred_class.astype(int)
+import numpy
+numpy.savetxt('results2.csv', y_pred_class, delimiter=',', header="y predicted Random Forest", comments="",fmt="%d")
+print('END  Random Forest Algorithm')
+########################################################################
+
+
+
